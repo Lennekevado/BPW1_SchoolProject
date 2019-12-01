770e00781ccf7abf8241a45b4488bfd6ec12a443
Len <Lennekevdo@gmail.com>
2019-12-01 14:43:47 +0100

    First Commit
    Added BPW 1 Finished unity project to Github

---
+++ BPW_1_LennekeVanDoorn/Assets/Script/Arrow_Projectile.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Arrow_Projectile : MonoBehaviour {
+	public float arrowForce;
+	void Start () {
+		GetComponent<Rigidbody>().AddForce(transform.forward * arrowForce, ForceMode.Impulse);
+	}
+}
---
+++ BPW_1_LennekeVanDoorn/Assets/Script/Character_Controller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_Controller : MonoBehaviour {
+	public GameObject arrow;
+	public Transform spawnPoint;
+	//public float arrowForce;
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0))
+		{
+			GameObject go = Instantiate(arrow, spawnPoint.position, Quaternion.identity);
+			//go.GetComponent<Rigidbody>().AddForce(transform.forward * arrowForce, ForceMode.Impulse);
+		}
+	}
+}
